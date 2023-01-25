ff4f516e81fa4c7cf396e5c82f99b5b92f22aef6
mypaxpax <58120765+mypaxpax@users.noreply.github.com>
2023-01-25 19:23:03 +0100

    Footer element

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,11 +1,15 @@
+import twitterIcon from "../assets/twitter-Icon.png";
+import instaIcon from "../assets/instagram-icon.png";
+import fbIcon from "../assets/Facebook-icon.png";
+import githubIcon from "../assets/Github-Icon.png";
+
 function Footer() {
   return (
-    <footer>
-      <img></img>
-      <img></img>
-      <img></img>
-      <img></img>
-      <img></img>
+    <footer className="footer-el">
+      <img src={twitterIcon}></img>
+      <img src={instaIcon}></img>
+      <img src={fbIcon}></img>
+      <img src={githubIcon}></img>
     </footer>
   );
 }
